b98b7538f5fdfa7537dabdb510cb66d351292769
G004S <senotrusov004@gmail.com>
2025-08-11 10:55:00 +0300

    Cars_comparison_oop

--- OOP/Cars_oop.py
+++ OOP/Cars_oop.py
@@ -1,3 +1,5 @@
+from functools import total_ordering
+@total_ordering
 class Car:
     wheels = 4 
     def __init__ (self, make, model, year):
@@ -9,12 +11,24 @@ class Car:
         return f"{self.year} {self.make} {self.model}, (wheels: {Car.wheels}) "
     def __repr__(self):
         return f"Car(make='{self.make}', model='{self.model}', year={self.year})"
+    
+    def __eq__(self, other):
+        if not isinstance (other, Car):
+            return NotImplemented
+        return (self.make, self.model, self.year) == (other.make, other.model, other.year)
+    def __lt__(self, other):
+        if not isinstance(other, Car):
+            return NotImplemented
+        return (self.year, self.make, self.model) < (other.year, other.make, other.model)
+
 
 cars = [
     Car("Toyota", "Corolla", 2020),
     Car("Honda", "Civic", 2019),
     Car("Ford", "Mustang", 2021),
-    Car("Chevrolet", "Camaro", 2022)
+    Car("Ford", "Kucha", 2022),
+    Car("VW", "GOLF", 2002)
 ]
-for car in cars:
+
+for car in sorted(cars, reverse=True):
     print(car)
